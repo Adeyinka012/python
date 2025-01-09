6f4dc3aa6ca299ce20d4d5c0b6fba87c31060755
Adeyinka012 <hassee4u@gmail.com>
2025-01-09 19:59:06 +0100

    space

---
+++ us.py
@@ -0,0 +1,12 @@
+print("Star pattern \n ")
+for i in range(1,6):
+    for j in range(i):
+        print('*', end="")
+    print('\n')    
+print("Invert Star pattern \n")
+for i in range(24,1,-1):
+    for j in range(i,1,-1):
+        print('*', end="")
+    print('\n')    
+    
+        
