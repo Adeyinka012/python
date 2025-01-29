0ea66740a3068cd856221d40dc28264cfa13ecf3
Adeyinka012 <hassee4u@gmail.com>
2025-01-29 20:03:28 +0100

    classic

--- file.py
+++ file.py
@@ -7,4 +7,8 @@ for x in file:
     print(x)
 x = open('file.txt', 'a')
 x.write("\n Pro coder")    
-    
+with open("file.txt", "r") as file:
+    data =file.readlines()
+    for line in data:
+        x =line.split()
+        print(x)    
